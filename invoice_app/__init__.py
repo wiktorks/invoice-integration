f974d789ed9758fee5a76af45cb7f9470a91dd06
wiktorks <wiktorks1994@gmail.com>
2021-11-13 17:02:02 +0100

    Project refactor

--- invoice_app/__init__.py
+++ invoice_app/__init__.py
@@ -1,34 +1,15 @@
-from flask import Flask, redirect, url_for
-from flask_jwt_extended import (
-    JWTManager,
-)
-from invoice_app.routes.main import main
-from invoice_app.routes.auth import auth
-from invoice_app.config import DevConfig
-
-jwt = JWTManager()
-
-
-@jwt.unauthorized_loader
-def custom_unauthorized_response(_err):
-    return redirect(url_for("auth.logout"))
-
-
-@jwt.expired_token_loader
-def custom_expired_response(header, payload):
-    return redirect(url_for("auth.logout"))
-
-
-@jwt.user_lookup_loader
-def user_lookup_callback(_jwt_header, jwt_data):
-    identity = jwt_data["sub"]
-    return {"name": identity}
+from flask import Flask
+from .routes.main import main
+from .routes.auth import auth
+from .config import DevConfig
+from .extensions import jwt, mail
 
 
 def create_app(config_class=DevConfig):
     app = Flask(__name__, static_url_path="/public", static_folder="public")
     app.config.from_object(config_class)
     jwt.init_app(app)
+    mail.init_app(app)
 
     app.register_blueprint(main)
     app.register_blueprint(auth)
--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 from invoice_app import create_app
-
-from invoice_app.config import ProdConfig, DevConfig
+from invoice_app.config import ProdConfig
 
 app = create_app(config_class=ProdConfig)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
