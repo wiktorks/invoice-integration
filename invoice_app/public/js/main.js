139b37d8986b6c6a63ca1a7c4de528fab66a24bb
wiktorks <wiktorks1994@gmail.com>
2021-11-16 10:51:08 +0100

    Fix UI calendar issues

--- invoice_app/public/js/main.js
+++ invoice_app/public/js/main.js
@@ -35,12 +35,35 @@ for (element of checkboxList) {
 
 toggleInvoiceButtonDisplay();
 
+const urlParams = new URLSearchParams(window.location.search);
+const startDateQuery = urlParams.get('start_date')
+const endDateQuery = urlParams.get('end_date')
+let endDate = endDateQuery ? endDateQuery: null
+let startDate;
+if (endDate) {
+  startDate  = startDateQuery ? startDateQuery: new Date(endDate.setMonth(endDate.getMonth()-1));
+} else {
+  const date_today = new Date()
+  startDate  = startDateQuery ? startDateQuery: new Date(date_today.setMonth(date_today.getMonth()-1));
+}
 const picker = new Litepicker({
   element: document.getElementById("litepicker"),
   singleMode: false,
   numberOfMonths: 2,
   numberOfColumns: 2,
-  inlineMode: true
+  inlineMode: true,
+  startDate: startDate,
+  endDate: endDate ? endDate: new Date()
 });
 picker.show();
+
+const filterByDate = () => {
+  start = picker.getStartDate()
+  end = picker.getEndDate()
+
+  window.location.href = window.location.origin + `?start_date=${start.getTime()}&end_date=${end.getTime()}`
+}
+
+calendarButton = document.getElementById('filter-by-date')
+calendarButton.addEventListener("click", filterByDate)
 
