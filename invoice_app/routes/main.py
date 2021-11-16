a75dcebf7ddb4f2b97798a370518b5b1404fd149
wiktorks <wiktorks1994@gmail.com>
2021-11-16 03:18:14 +0100

    Replace excel reader with api reader

--- invoice_app/routes/main.py
+++ invoice_app/routes/main.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, current_app
 from flask_jwt_extended import jwt_required, current_user
 from flask_mailing import Message
-from ..utils.excel_reader import ExcelReader
+from ..utils.clickup_reader import ClickupReader
 from ..extensions import mail
 import os
 
@@ -13,17 +13,12 @@ main = Blueprint("main", __name__)
 @jwt_required(optional=True)
 def index():
     if current_user:
-        excel_path = os.path.join(
-            current_app.root_path, "daily_report_28_10_2021_v2.xlsx"
-        )
-        er = ExcelReader(excel_path)
-        invoice_data = er.get_total_hours_by_project()
+        cr = ClickupReader()
+        invoice_data = cr.get_billed_tasks()
         return render_template(
             "index.html",
             invoice_data=invoice_data,
             current_user=current_user,
-            session="True" if current_app.config["SESSION_COOKIE_SECURE"] else "False",
-            jwt="True" if current_app.config["JWT_COOKIE_SECURE"] else "False",
         )
     else:
         flash("You must log in to use this app.", "info")
