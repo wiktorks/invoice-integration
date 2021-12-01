b91873923b594119ec758cb895b6ab9226b8fd30
wiktorks <wiktorks1994@gmail.com>
2021-12-01 13:15:31 +0100

    Create multi report display for front end

--- invoice_app/public/js/main.js
+++ invoice_app/public/js/main.js
@@ -3,7 +3,12 @@ import {
   toggleInvoiceButtonDisplay,
 } from "./modules/checkboxes.js";
 import { picker, filterByDate } from "./modules/calendar.js";
-import { viewMail, sendInvoiceMail } from "./modules/mailer.js";
+import {
+  viewMail,
+  sendInvoiceMail,
+  viewMails,
+  sendMails,
+} from "./modules/mailer.js";
 
 const mainCheckbox = document
   .getElementsByClassName("invoice-main-checkbox")
@@ -14,12 +19,21 @@ const checkboxList = document.getElementsByClassName("invoice-checkbox");
 const mailViewButtons = document.getElementsByClassName("mail-sender");
 const mailSendButton = document.getElementsByClassName("send-mail")[0];
 const calendarButton = document.getElementById("filter-by-date");
-
+const multipleMailViewButtons = document.getElementsByClassName(
+  "multiple-email-sender"
+);
+const multipleMailSendButton =
+  document.getElementsByClassName("send-multiple-mail")[0];
 
 for (let element of mailViewButtons) {
   element.addEventListener("click", viewMail);
 }
+
+for (let element of multipleMailViewButtons) {
+  element.addEventListener("click", viewMails);
+}
 mailSendButton.addEventListener("click", sendInvoiceMail);
+multipleMailSendButton.addEventListener("click", sendMails);
 
 mainCheckbox.addEventListener("change", toggleAllCheckboxes);
 mainCheckbox.addEventListener("change", toggleInvoiceButtonDisplay);
@@ -33,6 +47,5 @@ for (let mainTaskCheckbox of mainTaskCheckboxList) {
 toggleInvoiceButtonDisplay();
 
 picker.show();
-
 
 calendarButton.addEventListener("click", filterByDate);
